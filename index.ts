b633323c22a6c83d6c4bd31951149f30e85d5af8
ManuAngot <manu.angot@hotmail.fr>
2022-11-24 11:23:59 +0100

    création dossier TP TypeScript et fichiers

---
+++ index.ts
@@ -0,0 +1,7 @@
+const prenom : string = "Michel";
+console.log("bonjour " + prenom);
+
+let num1 : number = 1;
+let num2 : number = 2;
+console.log("Le nombre 1 = " + num1 + ", le nombre 2 = "+ num2);
+
